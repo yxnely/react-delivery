99188e6e7916a89cfa491e63d1c13c0b0cf44950
Yanely <yanely_rmz@yahoo.com>
2018-07-21 20:11:02 -0400

    formatting the search form

--- src/scripts/Donate/Search.js
+++ src/scripts/Donate/Search.js
@@ -4,14 +4,14 @@ const Search = () => {
 	return (
 		<form>
 			<div className="form-row">
-				<div className="col-12">
-					<label className="sr-only" htmlFor="inlineFormInputGroup">Search</label>
-					<div className="input-group mb-2">
-						<div className="input-group-prepend">
-							<div className="input-group-text">Search</div>
-						</div>
-						<input type="text" className="form-control" id="inlineFormInputGroup" placeholder="Search" />
+				<div className="col-sm-11 mb-2">
+					<label className="sr-only" htmlFor="search">Search</label>
+					<div className="input-group">
+						<input type="text" className="form-control" id="search" placeholder="Enter item name" />
 					</div>
+				</div>
+				<div className="col-sm-1 mb-2">
+					<button type="submit" className="btn btn-primary">Search</button>
 				</div>
 			</div>
 		</form>
--- src/scripts/Donate/quickOptions.js
+++ src/scripts/Donate/quickOptions.js
@@ -4,14 +4,91 @@ class options extends React.Component {
 	constructor() {
     super();
     this.state = {};
-    this.options = []
+    this.options = [
+			{
+				"id": 1,
+				"name": "Socks",
+				"price": 2.99
+			},
+			{
+				"id": 2,
+				"name": "Shoes",
+				"price": 9.99
+			},
+			{
+				"id": 3,
+				"name": "Toothbrush",
+				"price": 0.99
+			},
+			{
+				"id": 4,
+				"name": "Toothpaste",
+				"price": 1.99
+			},
+			{
+				"id": 5,
+				"name": "Brush",
+				"price": 1.99
+			}
+			,
+			{
+				"id": 6,
+				"name": "Shirt",
+				"price": 3.99
+			},
+			{
+				"id": 7,
+				"name": "Jeans",
+				"price": 8.99
+			},
+			{
+				"id": 8,
+				"name": "Deodorant",
+				"price": 1.99
+			},
+			{
+				"id": 9,
+				"name": "Shampoo",
+				"price": 1.99
+			},
+			{
+				"id": 10,
+				"name": "Mittens",
+				"price": 3.99
+			},
+			{
+				"id": 11,
+				"name": "Blanket",
+				"price": 10.49
+			},
+			{
+				"id": 12,
+				"name": "Canned Food",
+				"price": 5.00
+			},
+			{
+				"id": 13,
+				"name": "Water",
+				"price": 1.99
+			},
+			{
+				"id": 14,
+				"name": "Canned Food",
+				"price": 5.00
+			},
+			{
+				"id": 15,
+				"name": "Water",
+				"price": 1.99
+			}
+		]
 	}
 
 	render() {
 		return (
-			<div className="d-flex justify-content-between">
+			<div className="d-flex flex-wrap justify-content-between">
 				{this.options.map(opt => (
-					<button key={opt.id} type="button" className="btn btn-primary mb-1">
+					<button key={opt.id} type="button" className="col-md-2 btn btn-light mb-3 mr-2">
 						{opt.name}
 					</button>
 				))}
