9221b2a8186615b7ad23b4eeb54d2830274478d3
Yanely <yanely_rmz@yahoo.com>
2018-07-22 15:19:41 -0400

    add: fetch & making rqsts

--- src/scripts/Donate/quickOptions.js
+++ src/scripts/Donate/quickOptions.js
@@ -1,91 +1,25 @@
 import React from 'react';
 import Rqst from '../Service/Request';
+import 'whatwg-fetch';
 
 class options extends React.Component {
 	constructor() {
     super();
     this.state = {
-			isChosen: false
+			isChosen: false,
+			options: [],
 		};
-    this.options = [
-			{
-				"id": 1,
-				"name": "Socks",
-				"price": 2.99
-			},
-			{
-				"id": 2,
-				"name": "Shoes",
-				"price": 9.99
-			},
-			{
-				"id": 3,
-				"name": "Toothbrush",
-				"price": 0.99
-			},
-			{
-				"id": 4,
-				"name": "Toothpaste",
-				"price": 1.99
-			},
-			{
-				"id": 5,
-				"name": "Brush",
-				"price": 1.99
-			}
-			,
-			{
-				"id": 6,
-				"name": "Shirt",
-				"price": 3.99
-			},
-			{
-				"id": 7,
-				"name": "Jeans",
-				"price": 8.99
-			},
-			{
-				"id": 8,
-				"name": "Deodorant",
-				"price": 1.99
-			},
-			{
-				"id": 9,
-				"name": "Shampoo",
-				"price": 1.99
-			},
-			{
-				"id": 10,
-				"name": "Mittens",
-				"price": 3.99
-			},
-			{
-				"id": 11,
-				"name": "Blanket",
-				"price": 10.49
-			},
-			{
-				"id": 12,
-				"name": "Canned Food",
-				"price": 5.00
-			},
-			{
-				"id": 13,
-				"name": "Water",
-				"price": 1.99
-			},
-			{
-				"id": 14,
-				"name": "Canned Food",
-				"price": 5.00
-			},
-			{
-				"id": 15,
-				"name": "Water",
-				"price": 1.99
-			}
-		];
 		this.list = [];
+	}
+
+	componentDidMount() {
+		fetch('https://raw.githubusercontent.com/yxnely/react-delivery/master/src/data/options.json', {
+			method: 'GET',
+		})
+		.then(res => res.json())
+		.then((data) => {
+			this.setState({ options: data.options });
+		});
 	}
 
 	handleClick(evt) {
@@ -93,7 +27,7 @@ class options extends React.Component {
 		
 		const selectedItem = Number(evt.target.value);
 
-		this.addItem(this.options[selectedItem - 1], this.setBtn, evt);
+		this.addItem(this.state.options[selectedItem - 1], this.setBtn, evt);
 	}
 
 	setBtn(btn) {
@@ -117,7 +51,7 @@ class options extends React.Component {
 	render() {
 		return (
 			<div className="d-flex flex-wrap justify-content-between">
-				{this.options.map(opt => (
+				{this.state.options.map(opt => (
 					<button
 						onClick={e => this.handleClick(e)}
 						key={opt.id}
